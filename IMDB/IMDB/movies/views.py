5e5b03240a30d633d5bc92c32e7fb719f36deab5
Dipen <dmadroja@gmail.com>
2015-06-02 09:16:16 +0530

    fix for genre get

--- IMDB/IMDB/movies/views.py
+++ IMDB/IMDB/movies/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from models import Movies
+from models import Movies, Genre
 from serializers import MoviesSerializer,GenreSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import permission_classes
