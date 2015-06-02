e67abaedb0d50c27040feb3f539d9722ea43aa1a
Dipen <dmadroja@gmail.com>
2015-06-02 12:58:11 +0530

    Heroku changes

--- IMDB/IMDB/settings.py
+++ IMDB/IMDB/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
-STATICFILES_DIRS = (
-os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+# STATICFILES_DIRS = (
+# os.path.join(PROJECT_ROOT, 'static'),
+# )
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
